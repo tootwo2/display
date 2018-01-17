acd8d17b17c0009ddffe3219aada73a91ed36f40
tootwo2 <roc_tootwo@163.com>
2018-01-17 13:46:18 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,25 +6,25 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(version).then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/js/jquery.min.js',
-        '/js/bootstrap.min.js',
-        '/js/bootstrap-table.min.js',
-        '/js/bootstrap-table-zh-CN.min.js',
-        '/js/bootstrapValidator.min.js',
-        '/js/jquery.rotate.js',
-        '/js/nkimagemove1.js',
-        '/css/bootstrap.min.css',
-        '/css/bootstrap-table.min.css',
-        '/css/bootstrapValidator.min.css',
-        '/css/nkimagemove.css',
-        '/img/333824-021.png',
-        '/img/898466-011.png',
-        '/img/903896-001.png',
-        '/img/904695-003.png',
-        '/img/904695-005.png',
-        '/fonts/glyphicons-halflings-regular.woff2'
+        '/display/',
+        '/display/index.html',
+        '/display/js/jquery.min.js',
+        '/display/js/bootstrap.min.js',
+        '/display/js/bootstrap-table.min.js',
+        '/display/js/bootstrap-table-zh-CN.min.js',
+        '/display/js/bootstrapValidator.min.js',
+        '/display/js/jquery.rotate.js',
+        '/display/js/nkimagemove1.js',
+        '/display/css/bootstrap.min.css',
+        '/display/css/bootstrap-table.min.css',
+        '/display/css/bootstrapValidator.min.css',
+        '/display/css/nkimagemove.css',
+        '/display/img/333824-021.png',
+        '/display/img/898466-011.png',
+        '/display/img/903896-001.png',
+        '/display/img/904695-003.png',
+        '/display/img/904695-005.png',
+        '/display/fonts/glyphicons-halflings-regular.woff2'
       ]);
     })
   );
